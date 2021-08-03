e86326aaf62dad3cbb25f1feaaaa8ee8a24cb926
XJDHDR <XJDHDR@users.noreply.github.com>
2021-08-03 16:12:34 +1200

    Resized and recoloured the program's UI
    - The window opened by the program was too small so I expanded it.
    - Recoloured all UI elements that can't be interacted with to be light grey. In particular, the text boxes and text describing those boxes are now different colours.

--- zeus_and_poseidon/Properties/AssemblyInfo.cs
+++ zeus_and_poseidon/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Zeus & Poseidon Resolution Customiser")]
-[assembly: AssemblyCopyright("Copyright © XJDHDR - MIT license - 2021")]
+[assembly: AssemblyCopyright("Copyright © 2021 - XJDHDR - MIT license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
